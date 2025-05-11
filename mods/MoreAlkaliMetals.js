dbd0482a888a3c799d3ce5bf111f589804182e43
ThePigGaming2 <thepiggamingreal@gmail.com>
2025-05-10 20:33:31 -0400

    Update MoreAlkaliMetals.js

--- mods/MoreAlkaliMetals.js
+++ mods/MoreAlkaliMetals.js
@@ -6,13 +6,25 @@ elements.Lithium = {
     tempHigh: 180.5,
     stateHigh: "Molten_Lithium",
     density: 534,
+    reactions: water={elem1:"explosion",elem2:"hydrogen",
+    reactions: sugar_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: pool_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: dirty_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: salt_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: seltzer_water={elem1:"explosion",elem2:"hydrogen",
 }
 
 elements.Molten_Lithium = {
-	color: "#C0C0C0",
+	color: "#E37B53",
     behavior: behaviors.MOLTEN,
     fireColor: "#FF0038",
     density: 534,
     tempLow: 180.5,
     stateLow: "Lithium",
+    reactions: water={elem1:"explosion",elem2:"hydrogen",
+    reactions: sugar_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: pool_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: dirty_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: salt_water={elem1:"explosion",elem2:"hydrogen",
+    reactions: seltzer_water={elem1:"explosion",elem2:"hydrogen",
 }
